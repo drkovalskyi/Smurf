018f7792f8f919b715dd7a75885b6cabf4780554
dlevans <>
2011-06-13 15:15:28 +0000

    default libgfortran for the 4_2_X release

--- ME/rootlogon.C
+++ ME/rootlogon.C
@@ -1,6 +1,6 @@
 {
   gSystem->SetIncludePath("-I$ROOTSYS/include"); 
-  gSystem->Load("./libgfortran.so");
+  gSystem->Load("libgfortran.so");
   gSystem->Load("libPhysics.so");
   gSystem->Load("libEG.so");
   gSystem->Load("./libmcfm.so");
