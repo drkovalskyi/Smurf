bb046bc6943d0cd936140cc63c34fe501cd546ef
ceballos <>
2011-06-30 07:54:13 +0000

    new

--- MVA/run_mva.sh
+++ MVA/run_mva.sh
@@ -12,37 +12,48 @@ export NJETS=$1;
 export MH=$2;
 
 export TAG=ntuples_${MH}train_${NJETS}jets;
-export METHODS=KNN,BDT,BDTD,MLPBNN,BDTG;
+#export METHODS=KNN,BDT,BDTD,MLPBNN,BDTG;
+export METHODS=MLPBNN,BDTG;
 
 ### Training: change done hand made, it's an expert option
 export trainMVA_smurfFile=trainMVA_smurf.C+;
 if [ ${NJETS} == "1" ]; then
   export trainMVA_smurfFile=trainMVA_smurf1.C+;
+elif [ ${NJETS} == "hzz" ]; then
+  export trainMVA_smurfFile=trainMVA_smurf_hzz.C+;
+  export NJETS=999;
 fi
+
 export DO_TRAINING=0;
 if [ ${DO_TRAINING} == "1" ]; then
-  export SIG_TRAIN=data/hww${MH}.root;
-  export BKG_TRAIN=data/histo_hww_train_pu.root;
+  export SIG_TRAIN=data/../mitf-alljets-hzz/hzz${MH}.root;
+  export BKG_TRAIN=data/zz.root;
+  #export BKG_TRAIN=data/background_training.root;
+  #export SIG_TRAIN=data/hww${MH}.root;
   #export BKG_TRAIN=data/ww2l_pythia.root;
   mkdir -p weights;
   root -l -q -b ${trainMVA_smurfFile}\(${NJETS},\"${SIG_TRAIN}\",\"${BKG_TRAIN}\",\"${TAG}\",\"${METHODS}\",${MH}\);
 fi
+#exit;
 
 ### Fill MVA output information
 ### MVA output is available in "weights" folder
 ### an arbitrary list of samples can be added
 ### samples must be in "data" folder
 rm -f list_samples.txt;
+#data/data_2l.root
+#data/hww${MH}.root
+#data/background_mconly.root
+#data/background.root
 cat > list_samples.txt <<EOF
-data/hww${MH}.root
-data/data.root
-data/histo_hww_std_data_pu.root
-data/histo_hww_std_pu.root
+data/zz.root
 EOF
 
 export evaluateMVAFile=evaluateMVA_smurf.C+;
 if [ ${NJETS} == "1" ]; then
   export evaluateMVAFile=evaluateMVA_smurf1.C+;
+elif [ ${NJETS} == "999" ]; then
+  export evaluateMVAFile=evaluateMVA_smurf_hzz.C+;
 fi
 
 for i in `cat list_samples.txt` ; do
