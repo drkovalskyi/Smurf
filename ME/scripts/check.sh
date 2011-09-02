fb9e128b971ca7ace51381733de418f9521548a5
dlevans <>
2011-09-02 16:51:32 +0000

    first commit of script to check for missing output

---
+++ ME/scripts/check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# input is process name to check
+
+PROC=$1
+
+while read LINE; do 
+
+	TEST=`echo $LINE | grep Run2011`
+	if [ ! -z "$TEST" ]; then
+        	SECTION=`echo $LINE | awk '{print $3}'`
+        	FILE=`ls | grep ${PROC}_ME_${SECTION}.root`
+        	if [ -z "$FILE" ]; then
+        	        echo ${PROC}_ME_${SECTION}.root is missing
+	        fi
+	fi
+
+done < commands_${PROC}.cmd
+
