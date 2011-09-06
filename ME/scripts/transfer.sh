32d95818a969fb6f67db2ad05351ac43b7f34a5a
yygao <>
2011-09-06 17:24:15 +0000

    copy files from lpc to cern

---
+++ ME/scripts/transfer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# You need to do kinit -A $USER@FNAL.GOV first to excute this script
+
+export OUTDIR=/smurf/yygao/data/LP2011/WW/0j/ME/
+
+#for PROC in data hww115 hww120 hww130 hww140 hww150 hww160 hww170 hww180 hww190 hww200 hww250 hww300 qqww ggww wjets wjets_data wjets_PassFail ttbar tw dyee dymm dytt wgamma wz zz; do
+for PROC in data ww2l_pythia; do
+    scp ygao@cmslpc-sl5.fnal.gov:/uscms_data/d2/ygao/SmurfV6/WW/LP2011/ME/{$PROC}_ME.root $OUTDIR/${PROC}.root
+    cp $OUTDIR/${PROC}.root $OUTDIR/${PROC}_dXsec.root
+done
