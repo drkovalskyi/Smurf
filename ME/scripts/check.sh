f463cd4418ba03af420b9344b3f062196029bdbd
yygao <>
2011-09-06 09:22:35 +0000

    add intepretation

--- ME/scripts/check.sh
+++ ME/scripts/check.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # input is process name to check
+# This the script to compare the number of jobs submitted and the output to make sure no job is missing
 
 PROC=$1
+
+if [ ! $# -eq 1 ]; then
+    echo "USAGE: ./check.sh proc
+        proc - the process name e.g. hww120"
+    exit 1
+fi
+
 
 while read LINE; do 
 	TEST=`echo $LINE | grep "tardir/cafRun"`
