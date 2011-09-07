3d975cb9187571dcbb0a848f5987e0a23bd6d1f3
yygao <>
2011-09-07 08:38:57 +0000

    adpting to the LP datacards

--- LimitCalc/splitcard.C
+++ LimitCalc/splitcard.C
@@ -25,10 +25,11 @@ void splitcard(int mH = 140, int njet = 0, const char *flavor = "of", TString in
   TH1F *histo_Zjets = (TH1F*)file->Get("histo_Zjets");
   TH1F *histo_VV = (TH1F*)file->Get("histo_VV");
   TH1F *histo_Top = (TH1F*)file->Get("histo_Top");
+  TH1F *histo_Ztt = (TH1F*)file->Get("histo_Ztt");
 
   histcheck(histo_Wjets);
-  float nZH(0.0), nWH(0.0), nqqH(0.0), nggH(0.0), nqqWW(0.0), nggWW(0.0), nWjets(0.0), nWgamma(0.0), nZjets(0.0), nVV(0.0), nTop(0.0);
-  float sigmanZH(0.0), sigmanWH(0.0), sigmanqqH(0.0), sigmanggH(0.0), sigmanqqWW(0.0), sigmanggWW(0.0), sigmanWjets(0.0), sigmanWgamma(0.0), sigmanZjets(0.0), sigmanVV(0.0), sigmanTop(0.0);
+  float nZH(0.0), nWH(0.0), nqqH(0.0), nggH(0.0), nqqWW(0.0), nggWW(0.0), nWjets(0.0), nWgamma(0.0), nZjets(0.0), nVV(0.0), nTop(0.0), nZtt(0.0);
+  float sigmanZH(0.0), sigmanWH(0.0), sigmanqqH(0.0), sigmanggH(0.0), sigmanqqWW(0.0), sigmanggWW(0.0), sigmanWjets(0.0), sigmanWgamma(0.0), sigmanZjets(0.0), sigmanVV(0.0), sigmanTop(0.0), sigmanZtt(0.0);
   
   for (int i = 1; i < histo_Data->GetNbinsX()+1; i++ ) {
     
@@ -43,6 +44,7 @@ void splitcard(int mH = 140, int njet = 0, const char *flavor = "of", TString in
     getbin(histo_Zjets, i, nZjets, sigmanZjets);
     getbin(histo_VV, i, nVV, sigmanVV);
     getbin(histo_Top, i, nTop, sigmanTop);
+    getbin(histo_Ztt, i, nZtt, sigmanZtt);
     
     
     ifstream inputfile(shapecard);
@@ -61,32 +63,34 @@ void splitcard(int mH = 140, int njet = 0, const char *flavor = "of", TString in
 	    else if (TString(line).Contains("shapes", TString::kExact)) 
 	      newcard << "";
 	    else if (TString(line).Contains("rate", TString::kExact))
-	      newcard << Form("rate  %.3f   %.3f   %.3f   %.3f  %.3f  %.3f   %.3f   %.3f   %.3f  %.3f   %.3f\n", 
-			      nZH, nWH, nqqH, nggH, nqqWW, nggWW, nVV, nTop, nZjets, nWjets, nWgamma);
+	      newcard << Form("rate  %.3f   %.3f   %.3f   %.3f  %.3f  %.3f   %.3f   %.3f   %.3f  %.3f   %.3f   %.3f\n", 
+			      nZH, nWH, nqqH, nggH, nqqWW, nggWW, nVV, nTop, nZjets, nWjets, nWgamma, nZtt);
 	
 	    else if (TString(line).Contains("stat", TString::kExact)) {
 	      if (TString(line).Contains("ZH", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_ZH_bin%i     lnN %4.3f   -     -     -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanZH);
+		newcard << Form("CMS_hww%s_stat_%ij_ZH_bin%i     lnN %4.3f   -     -     -     -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanZH);
 	      else if (TString(line).Contains("WH", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_WH_bin%i     lnN   -   %4.3f   -     -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanWH);
-	      else if (TString(line).Contains("qqH", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_qqH_bin%i    lnN   -     -   %4.3f   -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanqqH);
-	      else if (TString(line).Contains("ggH", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_ggH_bin%i    lnN   -     -     -   %4.3f   -     -     -     -     -     -     -   \n", flavor, njet, i, 1.0+sigmanggH);
-	      else if (TString(line).Contains("WW", TString::kExact) && !(TString(line).Contains("ggWW", TString::kExact)))
-		newcard << Form("CMS_hww%s_stat_%ij_WW_bin%i     lnN   -     -     -     -   %4.3f   -     -     -     -     -     -   \n", flavor, njet, i, 1.0+sigmanqqWW);
-	      else if (TString(line).Contains("ggWW", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_ggWW_bin%i   lnN   -     -     -     -     -   %4.3f   -     -     -     -     -   \n", flavor, njet, i, 1.0+sigmanggWW);
-	      else if (TString(line).Contains("VV", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_VV_bin%i     lnN   -     -     -     -     -     -   %4.3f   -     -     -     -   \n", flavor, njet, i, 1.0+sigmanVV);
-	      else if (TString(line).Contains("ttbar", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_ttbar_bin%i  lnN   -     -     -     -     -     -     -   %4.3f   -     -     -   \n", flavor, njet, i, 1.0+sigmanTop);
-	      else if (TString(line).Contains("Z", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_Z_bin%i      lnN   -     -     -     -     -     -     -     -   %4.3f   -     -   \n", flavor, njet, i, 1.0+sigmanZjets);
-	      else if (TString(line).Contains("Wjets", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_Wjets_bin%i  lnN   -     -     -     -     -     -     -     -     -   %4.3f   -    \n", flavor, njet, i, 1.0+sigmanWjets);
-	      else if (TString(line).Contains("Wgamma", TString::kExact))
-		newcard << Form("CMS_hww%s_stat_%ij_Wgamma_bin%i lnN   -     -     -     -     -     -     -     -     -     -   %4.3f  \n", flavor, njet, i, 1.0+sigmanWgamma);
+		newcard << Form("CMS_hww%s_stat_%ij_WH_bin%i     lnN   -   %4.3f   -     -     -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanWH);
+	      else if (TString(line).Contains("qqH_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_qqH_bin%i    lnN   -     -   %4.3f   -     -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanqqH);
+	      else if (TString(line).Contains("ggH_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_ggH_bin%i    lnN   -     -     -   %4.3f   -     -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanggH);
+	      else if (TString(line).Contains("WW_", TString::kExact) && !(TString(line).Contains("ggWW", TString::kExact)))
+		newcard << Form("CMS_hww%s_stat_%ij_WW_bin%i     lnN   -     -     -     -   %4.3f   -     -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanqqWW);
+	      else if (TString(line).Contains("ggWW_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_ggWW_bin%i   lnN   -     -     -     -     -   %4.3f   -     -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanggWW);
+	      else if (TString(line).Contains("VV_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_VV_bin%i     lnN   -     -     -     -     -     -   %4.3f   -     -     -     -     -  \n", flavor, njet, i, 1.0+sigmanVV);
+	      else if (TString(line).Contains("ttbar_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_ttbar_bin%i  lnN   -     -     -     -     -     -     -   %4.3f   -     -     -     -  \n", flavor, njet, i, 1.0+sigmanTop);
+	      else if (TString(line).Contains("Z_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_Z_bin%i      lnN   -     -     -     -     -     -     -     -   %4.3f   -     -     -  \n", flavor, njet, i, 1.0+sigmanZjets);
+	      else if (TString(line).Contains("Wjets_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_Wjets_bin%i  lnN   -     -     -     -     -     -     -     -     -   %4.3f   -     -  \n", flavor, njet, i, 1.0+sigmanWjets);
+	      else if (TString(line).Contains("Wgamma_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_Wgamma_bin%i lnN   -     -     -     -     -     -     -     -     -     -   %4.3f   -  \n", flavor, njet, i, 1.0+sigmanWgamma);
+	      else if (TString(line).Contains("Ztt_", TString::kExact))
+		newcard << Form("CMS_hww%s_stat_%ij_Ztt_bin%i    lnN   -     -     -     -     -     -     -     -     -     -     -   %4.3f \n", flavor, njet, i, 1.0+sigmanZtt);
 	      else 
 		newcard << line << endl;
 	    }
