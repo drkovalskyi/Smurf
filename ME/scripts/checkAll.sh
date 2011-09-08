0f630216a4e871cc9c0b7800a2c879fd4ae44dc7
yygao <>
2011-09-08 14:14:32 +0000

    check all processes

---
+++ ME/scripts/checkAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -f checklog.txt ]; then
+    rm -f checklog.txt
+fi
+
+for PROC in data hww115 hww120 hww130 hww140 hww150 hww160 hww170 hww180 hww190 hww200 hww250 hww300 qqww ggww wjets wjets_data wjets_PassFail ttbar tw dyee dymm dytt wgamma wz zz ww2l_pythia; do
+    echo "Checking " $PROC >> checkBatchjob.log 
+    ./check.sh $PROC >> checklog.txt 
+done
