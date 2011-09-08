32ce391a640e1bd92820aebdc9d80a5a16991603
yygao <>
2011-09-08 14:39:48 +0000

    default for the WW analysis

--- ME/cafRun.sh
+++ ME/cafRun.sh
@@ -14,6 +14,7 @@ NEV=$4
 EVSTART=$((NEV*($1-1)))
 HIGGSMASS=$5
 
+
 cd tardir
 
 echo "Event Start:" $EVSTART
@@ -21,10 +22,10 @@ echo "Copying $INPUTPATH/$ROOTFILE to the current folder"
 cp $INPUTPATH/$ROOTFILE ./
 
 echo "{" > temp.C
-#echo "gSystem->CompileMacro(\"runME_test.C\");" >> temp.C
-#echo "runME_test(\"./\", \"$ROOTFILE\", \"./\", 10, 1, 100000, 1.0, $NEV, $EVSTART,$HIGGSMASS);" >> temp.C
-echo "gSystem->CompileMacro(\"runME_HZZ.C\");" >> temp.C
-echo "runME_HZZ(\"./\", \"$ROOTFILE\", \"./\", 10, 1, 100000, 1.0, $NEV, $EVSTART,$HIGGSMASS);" >> temp.C
+echo "gSystem->CompileMacro(\"runME_test.C\");" >> temp.C
+echo "runME_test(\"./\", \"$ROOTFILE\", \"./\", 10, 1, 100000, 1.0, $NEV, $EVSTART);" >> temp.C
+#echo "gSystem->CompileMacro(\"runME_HZZ.C\");" >> temp.C
+#echo "runME_HZZ(\"./\", \"$ROOTFILE\", \"./\", 10, 1, 100000, 1.0, $NEV, $EVSTART,$HIGGSMASS);" >> temp.C
 echo "}" >> temp.C
 cat temp.C
 
