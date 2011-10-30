5bceb1eb0cbfbf7d32211010b55af4705b355f3c
yygao <>
2011-10-30 10:23:13 +0000

    update the mcnlo samples

---
+++ ME/scripts/submitAllZZ.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ data_2l.goodlumi.Run2011B_BTAG /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ data_2l.goodlumi.Run2011B /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 8 1000 ZZ
+
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ data_2l.goodlumiRun2011A_BTAG /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ttbar2l-powheg_BTAG /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 199 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wtop-powheg_BTAG /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 2 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wtopb-powheg_BTAG /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 2 1000 ZZ
+
+
+#./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ data_2l.goodlumiRun2011A  /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 4 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ zz2l-madgraph /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 173 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wz3l-madgraph /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 54 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ww-madgraph /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 24 1000 ZZ
+
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ttbar2l-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 43 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wtop-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ttop-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wtopb-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ttopb-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ zee-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 8 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ zmm-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 9 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ztt-powheg /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 1 1000 ZZ
+
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ gfhzz250 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 27 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ vbfhzz250 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 13 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ gfhzz300 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 31 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ vbfhzz300 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 15 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ gfhzz350 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 33 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ vbfhzz350 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 11 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ gfhzz400 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 23 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ vbfhzz400 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 12 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ gfhzz500 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 35 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ vbfhzz500 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 16 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ gfhzz600 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 35 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ vbfhzz600 /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 16 1000 ZZ
+
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wz3l-pythia /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 14 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ zz2l-pythia /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 29 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ww-mcatnlo /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 10 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wwup-mcatnlo /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 10 1000 ZZ
+./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ wwdown-mcatnlo /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 10 1000 ZZ
+
+# not needed
+#./batchSubmit.sh /uscms/home/ygao/WWAnalyzsis/CMSSW_4_2_3_HZZ_Head/src/ ww2l-pythia /uscms_data/d2/ygao/HZZ2011A/ZZ/allj/ 4 1000 ZZ
