9b3f1306d1ec4cb8bcd04eaaef14d3b8700e7c54
ceballos <>
2011-10-31 21:36:20 +0000

    upgrading a cut

--- Analysis/HWWlvlv/TopBkgScaleFactors.h
+++ Analysis/HWWlvlv/TopBkgScaleFactors.h
@@ -1,12 +1,12 @@
 Double_t TopBkgScaleFactor(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactor[3] = { 1.35512, 1.1289, 1.15046   };
+  Double_t TopBkgScaleFactor[3] = { 1.36094, 1.11921, 1.16321   };
   return TopBkgScaleFactor[jetBin];
 }
 
 Double_t TopBkgScaleFactorKappa(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactorKappa[3] = { 1.21435, 1.06547, 1.03027   };
+  Double_t TopBkgScaleFactorKappa[3] = { 1.21404, 1.06515, 1.0299   };
   return TopBkgScaleFactorKappa[jetBin];
 }
 
