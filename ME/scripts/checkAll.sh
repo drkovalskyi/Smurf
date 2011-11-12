a3e956483baca18aad7742471876dc4dba2ddd5b
yygao <>
2011-11-12 17:10:52 +0000

    check all the files for full2011

--- ME/scripts/checkAll.sh
+++ ME/scripts/checkAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for PROC in data hww115 hww120 hww130 hww140 hww150 hww160 hww170 hww180 hww190 hww200 hww250 hww300 qqww ggww wjets wjets_data wjets_PassFail ttbar tw dyee dymm dytt wgamma wz zz ww2l_pythia; do
+for PROC in data data-emb-tau123 hww115 hww120 hww130 hww140 hww150 hww160 hww170 hww180 hww190 hww200 hww250 hww300 hww350 hww400 hww450 hww500 hww500 hww550 hww600 qqww ggww wjets wjets_data wjets_PassFail ttbar tw dyee dymm dytt wgamma wz zz_py ttbar_mg tw_ds wg3l dyee_LooseMET dymm_LooseMET ww_mcnlo ww_mcnlo_up ww_mcnlo_down; do
     echo "Checking " $PROC >> checkBatchjob.log 
     ./check.sh $PROC
 done
