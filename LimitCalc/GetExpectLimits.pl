9f650b0731ea5a302ec7e21005acb9c591545e65
dmytro <>
2011-12-12 14:06:46 +0000

    heavily changed version, close to the final one

--- LimitCalc/GetExpectLimits.pl
+++ LimitCalc/GetExpectLimits.pl
@@ -12,9 +12,11 @@ use warnings;
 my $lands = "../../LandS/test/lands.exe";
 
 my $useLSF = 1; 
-my $doObserved = 1;
+my $doObserved = 0;
 my $doExpected = 1;
 my $nJobsPerMassPoint = 10;
+# my $outputHostName = "cmswn1927.cern.ch";
+my $outputHostName = "lxplus.cern.ch";
 my $queue = "1nd"; # 1 day
 # my $queue = "cmscaf1nd"; # 1 day
 # my $queue = "cmscaf1nh"; # 1 hour
@@ -52,23 +54,16 @@ if ( $type eq "CLs-asymptotic" ){
 ## CLs
 ##
 if ( $type eq "CLs" ){
+    $nJobsPerMassPoint = 100;
     $jobname = "cls";
-    $expectLimitCommand = "-M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 1000 --nToysForCLb 500 --minuitSTRATEGY 1 --maximumFunctionCallsInAFit 50000 -rMin 0 -rMax 100"; 
-    $observedLimitCommand = "-M Hybrid --freq --minuitSTRATEGY 1 --maximumFunctionCallsInAFit 50000 -rMin 0 -rMax 100";
+    # GOOD $expectLimitCommand   = "-M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --nToysForCLsb 50 --nToysForCLb 25 --minuitSTRATEGY 1 --maximumFunctionCallsInAFit 50000 -rMin 0.01 -rMax 20"; 
+    $expectLimitCommand   = "-M Hybrid --freq -vR \"[0.2,5.0,0.2] [6.0,15.0,1]\" --scanRs 1 --nToysForCLsb 20 --nToysForCLb 10 --minuitSTRATEGY 1 --maximumFunctionCallsInAFit 50000"; 
+    $observedLimitCommand = "-M Hybrid --freq --nToysForCLsb 10000 --nToysForCLb 5000 --minuitSTRATEGY 1 --maximumFunctionCallsInAFit 50000 -rMin 0.01 -rMax 20";
+    $doObserved = 0;
 }
 
 die "Incorrect type $type\n" 
     unless (defined $expectLimitCommand && defined $observedLimitCommand);
-
-##
-## CLs
-##
-# my $expectLimitCommand = "-M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 1500 --nToysForCLb 500"; 
-# my $expectLimitCommand = "-M Hybrid --freq --ExpectationHints Asymptotic --scanRs 1 --freq --nToysForCLsb 4000 --nToysForCLb 2000"; 
-# my $expectLimitCommand = "-M Hybrid --freq --ExpectationHints Bayesian --scanRs 1 --freq --nToysForCLsb 10000 --nToysForCLb 5000"; 
-# my $expectLimitCommand = "-tB 1000 -tPB 30 -t 1000 -M Bayesian --doExpectation 1";
-# my $expectLimitCommand = "-tB 1000 -tPB 30 -t 100 -M Bayesian --doExpectation 1";
-# my $observedLimitCommand = "-tB 10000 -M Bayesian";
 
 sub seed{
     return int(rand()*10000);
@@ -93,35 +88,50 @@ foreach my $line (`cat $inputCard`){
 print "Number of mass points to process: ", scalar keys %cards,"\n";
 
 # make a wrapper script
-my $wrapper = "lands.sh";
+my $wrapper = "process_job.sh";
 my $path = `pwd`;
 $path =~ s/\n//;
-if ( ! -e $wrapper ){
-    open(OUT,">$wrapper") || die "Cannot create file $wrapper\n$!\n";
-    print OUT << "EOF";
+# if ( ! -e $wrapper ){
+open(OUT,">$wrapper") || die "Cannot create file $wrapper\n$!\n";
+print OUT << "EOF";
 #!/bin/sh
-source /afs/cern.ch/cms/sw/cmsset_default.sh
-cd $path
-eval `scram runtime -sh`
-$lands \$*
+# first argument is the output directory on the host
+# second is log file
+# the rest is for LandS
+workdir=$path
+remotehost="$outputHostName"
+lands=$path/$lands
 EOF
-    close OUT;
-    system("chmod u+x $wrapper");
-}
+print OUT << 'EOF';
+TMPDIR=`pwd`
+SCRATCH=`mktemp -d`
+remotedir=$1
+logfile=$2
+source /afs/cern.ch/cms/sw/cmsset_default.sh
+cd $workdir
+eval `scram runtime -sh`
+cd $SCRATCH
+$lands ${@:3} > $logfile 2>&1
+gzip $logfile
+scp * ${remotehost}:${remotedir} && rm *
+EOF
+close OUT;
+system "chmod u+x $wrapper";
 
-system("mkdir $dir/output") unless -d "$dir/output";
+system("mkdir $dir/output") unless -d "$dir/output" || -l "$dir/output";
+system("mkdir $dir/log") unless -e "$dir/log";
 foreach my $job(1..$nJobsPerMassPoint){
     my $seed = seed();
     foreach my $mass(sort {$a<=>$b} keys %cards){
 	my $cards = "";
 	foreach my $card(@{$cards{$mass}}){
-	    $cards .= " $dir/$card";
+	    $cards .= " $path/$dir/$card";
 	}
 	my $n = "$name-$mass-$jobname";
 	$n .= "-$seed" if ($nJobsPerMassPoint>1);
 	if ( $doExpected ) {
 	    if ($useLSF){
-		system("bsub -q $queue -o $dir/output/$n.log \"$wrapper $expectLimitCommand --seed $seed --name $dir/output/$n -d$cards\"");
+		system("bsub -q $queue -o $dir/log/$n.lsf \"$wrapper $path/$dir/output $n.log $expectLimitCommand --seed $seed --name $n -d$cards\"");
 	    } else {
 		system("nice $lands $expectLimitCommand --seed $seed --name $dir/output/$n -d$cards>$dir/output/$n.log 2>&1 &");
 	    }
@@ -130,7 +140,7 @@ foreach my $job(1..$nJobsPerMassPoint){
 	    # print "nice $lands $observedLimitCommand -d$cards>$dir/output/$name-$mass-$jobname.observed 2>&1 &\n";
 	    $n = "$name-$mass-$jobname";
 	    if ($useLSF){
-		system("bsub -q $queue -o $dir/output/$n.observed \"$wrapper $observedLimitCommand -d$cards\"");
+		system("bsub -q $queue -o $dir/log/$n.lsf \"$wrapper $path/$dir/output $n.observed $observedLimitCommand -d$cards\"");
 	    } else {
 		system("nice $lands $observedLimitCommand -d$cards>$dir/output/$name-$mass-$jobname.observed 2>&1 &");
 	    }
