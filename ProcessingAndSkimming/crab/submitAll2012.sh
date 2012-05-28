1a2c17a8934ca4c9f9602909d73e4b2677be8500
dlevans <>
2012-05-28 20:16:53 +0000

    script to submit all jobs

---
+++ ProcessingAndSkimming/crab/submitAll2012.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#crab -create -submit -cfg crab_DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball.cfg
+
+crab -create -submit -cfg crab_DoubleElectronRun2012APromptV1.cfg
+crab -create -submit -cfg crab_DoubleMu2012APromptV1.cfg
+crab -create -submit -cfg crab_SingleElectronRun2012APromptV1.cfg
+crab -create -submit -cfg crab_SingleMuRun2012APromptV1.cfg
+crab -create -submit -cfg crab_PhotonsRun2012APromptV1.cfg
+
+crab -create -submit -cfg crab_DoubleElectronRun2012BPromptV1.cfg
+crab -create -submit -cfg crab_DoubleMuRun2012BPromptV1.cfg
+crab -create -submit -cfg crab_SingleElectronRun2012BPromptV1.cfg
+crab -create -submit -cfg crab_SingleMuRun2012BPromptV1.cfg
+crab -create -submit -cfg crab_PhotonsRun2012BPromptV1.cfg
+
