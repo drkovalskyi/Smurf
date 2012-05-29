c22ecabc6d78829bd172d265c313c62f06b3e9bc
dlevans <>
2012-05-29 16:27:11 +0000

    update sandbox size.

--- ProcessingAndSkimming/crab/submitAll2012.sh
+++ ProcessingAndSkimming/crab/submitAll2012.sh
@@ -2,7 +2,7 @@
 
 #crab -create -submit -cfg crab_DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball.cfg
 
-crab -create -submit -cfg crab_DoubleElectronRun2012APromptV1.cfg
+#crab -create -submit -cfg crab_DoubleElectronRun2012APromptV1.cfg
 crab -create -submit -cfg crab_DoubleMu2012APromptV1.cfg
 crab -create -submit -cfg crab_SingleElectronRun2012APromptV1.cfg
 crab -create -submit -cfg crab_SingleMuRun2012APromptV1.cfg
