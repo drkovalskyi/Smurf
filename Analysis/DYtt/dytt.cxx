eedff49a2109ae2e68b7dd5dc47549cd259cab08
dmytro <>
2012-06-07 14:13:27 +0000

    one Ztt out of Zll should be good enough

--- Analysis/DYtt/dytt.cxx
+++ Analysis/DYtt/dytt.cxx
@@ -1,7 +1,7 @@
 {
   gSystem->CompileMacro("tauify.cc","k");
   gSystem->CompileMacro("dytt.C","k");
-  dytt(10,1.6,
+  dytt(1,1.6,
        "/smurf/data/Run2011_Spring11_SmurfV6_42X/mitf-alljets/dyee.root",dy::MCee,
        "/smurf/data/Run2011_Spring11_SmurfV6_42X/mitf-alljets/dytt.root");
 }
