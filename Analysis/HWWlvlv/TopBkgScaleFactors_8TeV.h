dcac279a65c29f20b44aa076e56208c65e7de7d3
ceballos <>
2012-06-14 07:38:18 +0000

    3.5ifb

--- Analysis/HWWlvlv/TopBkgScaleFactors_8TeV.h
+++ Analysis/HWWlvlv/TopBkgScaleFactors_8TeV.h
@@ -1,12 +1,12 @@
 static Double_t TopBkgScaleFactor(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactor[3] = { 1.0361, 1.18658, 1.42935   };
+  Double_t TopBkgScaleFactor[3] = { 1.054, 1.09757, 1.21142   };
   return TopBkgScaleFactor[jetBin];
 }
 
 static Double_t TopBkgScaleFactorKappa(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactorKappa[3] = { 1.22814, 1.07155, 1.3929   };
+  Double_t TopBkgScaleFactorKappa[3] = { 1.21555, 1.06004, 1.40087   };
   return TopBkgScaleFactorKappa[jetBin];
 }
 
