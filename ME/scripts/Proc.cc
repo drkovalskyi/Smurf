bc515abfd9b956a3a02419e95e5321f3e7e1aed5
yygao <>
2012-06-15 19:37:48 +0000

    update to 8 TeV

--- ME/scripts/Proc.cc
+++ ME/scripts/Proc.cc
@@ -57,7 +57,8 @@ void Proc::initYields() {
   if ( proc_ == TVar::WZ && analysis_ == HZZANALYSIS)  fileName = "wz3l-madgraph";
   if ( proc_ == TVar::ZZ && analysis_ == HZZANALYSIS)  fileName = "zz2l-madgraph";
   if ( proc_ >= TVar::HZZ200 && proc_ <= TVar::HZZ600 && analysis_ == HZZANALYSIS) fileName = "gf"+fileName;
- 
+  
+
   TFile *f = TFile::Open(TString(inputDir_ + fileName.Data() + ".root"));
   
   if(f==0x0) {
@@ -90,12 +91,13 @@ void Proc::initYields() {
   LorentzVector*  jet2_ = 0;
   LorentzVector*  jet3_ = 0;
   float metPhi_ = 0.;
-  unsigned int nvtx_ = 0;
   float pmet_ = 0.;
   float pTrackMet_ = 0.;
   LorentzVector*  lep2_ = 0;
-  
-  tree->SetBranchAddress( "nvtx",  &nvtx_);
+  float dymva_ = 0.;
+  unsigned int njets_ = 0;
+  float dPhiDiLepJet1_ = 0.;
+
   tree->SetBranchAddress( "type",  &type_);
   tree->SetBranchAddress( "scale1fb",  &scale1fb_);
   tree->SetBranchAddress( "dilep", &dilep_); 
@@ -108,6 +110,9 @@ void Proc::initYields() {
   tree->SetBranchAddress( "jet3", &jet3_);
   tree->SetBranchAddress( "metPhi",  &metPhi_); 
   tree->SetBranchAddress( "lep2", &lep2_); 
+  tree->SetBranchAddress( "dymva",  &dymva_);
+  tree->SetBranchAddress( "njets",  &njets_);
+  tree->SetBranchAddress( "dPhiDiLepJet1",  &dPhiDiLepJet1_);
 
   // start filling the numbers...
   
@@ -129,11 +134,18 @@ void Proc::initYields() {
     if ( analysis_ == HWWANALYSIS) {
       if ( dilep_->M() > massCut_ ) passpresel = false;
       if ( mt_ < 80.) passpresel = false;
+      if ( mt_ > mtCut_ ) passpresel = false;
       if ( dilep_->Pt() < 45) passpresel = false;
       if ( type_ == 0 || type_ == 3) {
-	if ( dilep_->M() < 20) passpresel = false;
-	if (TMath::Min(pmet_,pTrackMet_) < (37.+nvtx_/2.)  ) passpresel = false;
-	if ( lep2_->Pt() < 15) passpresel = false;
+	float metValue_ = TMath::Min(pmet_,pTrackMet_);
+	if ( proc_ >= TVar::HWW110  && proc_ <= TVar::HWW140 ) {
+	  if ( njets_ == 0 && dymva_ > 0.6 ) passpresel = false;
+	  if ( njets_ == 1 && dymva_ > 0.3 ) passpresel = false;
+	} else {
+	  if (metValue_ < 45. ) passpresel = false;
+	  if ( jet1_->Pt() > 15 && dPhiDiLepJet1_ > (165./180.*TMath::Pi()) ) 
+	    passpresel = false;
+	}
       }
     }
     
@@ -178,129 +190,287 @@ void Proc::initYields() {
   f->Close();
 }
 
+//
+// Note:: The MCFM xsections are updated with the MCFM6.2
+// The 7 TeV cross-sections are not done yet for mH>600.
+//
 
 void Proc::initTotXsec() {
   
+  bool EightTeV = false;
+
   switch (proc_) {
     
   case (TVar::WW): 
     NLOXsec_ = 4.5;
     MCFMXsec_ = 0.333;
+    if ( EightTeV ) {
+      NLOXsec_ = 4.5;
+      MCFMXsec_ = 0.4;
+    }
     break;
 
   case (TVar::Wp_1jet):
     NLOXsec_ = 31314.0;
     MCFMXsec_ = 18788.0/3.0;
+    if ( EightTeV ) {
+      NLOXsec_ = 4.5;
+      MCFMXsec_ = 21222./3.0;
+    }
     break;
 
   case (TVar::Wm_1jet):
     NLOXsec_ = 31314.0;
     MCFMXsec_ =  12525.0/3.0;
+    if ( EightTeV ) {
+      NLOXsec_ = 31314.0;
+      MCFMXsec_ =  14995.0/3.0;
+    }
+    break;
+
+  case (TVar::HWW110):
+    NLOXsec_ = 0.165009;
+    MCFMXsec_ = 0.003447;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.165009;
+      MCFMXsec_ = 0.00479;
+    }
+    
     break;
 
   case (TVar::HWW115):
     NLOXsec_ = 0.165009;
-    MCFMXsec_ = 0.005534;
+    MCFMXsec_ = 0.005754;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.165009;
+      MCFMXsec_ = 0.007462;
+    }
     break;
 
   case (TVar::HWW120):
     NLOXsec_ = 0.249642;
-    MCFMXsec_ = 0.008370;
+    MCFMXsec_ = 0.008743;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.249642;
+      MCFMXsec_ = 0.011372;
+    }
     break;
 
+  case (TVar::HWW125):
+    NLOXsec_ = 0.249642;
+    MCFMXsec_ = 0.012234;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.249642;
+      MCFMXsec_ = 0.015957;
+    }
+    break;
+    
   case (TVar::HWW130):
     NLOXsec_ = 0.452090;
-    MCFMXsec_ = 0.016125;
+    MCFMXsec_ = 0.0159;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.452090;
+      MCFMXsec_ = 0.0208;
+    }
+    break;
+
+ case (TVar::HWW135):
+   NLOXsec_ = 0.452090;
+   MCFMXsec_ = 0.01936;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.452090;
+      MCFMXsec_ = 0.0254;
+    }
     break;
 
   case (TVar::HWW140):
     NLOXsec_ = 0.641773;
-    MCFMXsec_ = 0.024054;
+    MCFMXsec_ = 0.02234;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.641773;
+      MCFMXsec_ = 0.0294;
+    }
+    break;
+
+  case (TVar::HWW145):
+    NLOXsec_ = 0.641773;
+    MCFMXsec_ = 0.025;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.641773;
+      MCFMXsec_ = 0.033;
+    }
     break;
 
   case (TVar::HWW150):
     NLOXsec_ = 0.770471;
-    MCFMXsec_ = 0.029674;
+    MCFMXsec_ = 0.02674;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.770471;
+      MCFMXsec_ = 0.0354;
+    }
     break;
+
+  case (TVar::HWW155):
+    NLOXsec_ = 0.770471;
+    MCFMXsec_ = 0.0281;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.770471;
+      MCFMXsec_ = 0.0373;
+    }
+    break;
+
 
   case (TVar::HWW160):
     NLOXsec_ = 0.866443;
-    MCFMXsec_ = 0.033716;
+    MCFMXsec_ = 0.0293;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.782962;
+      MCFMXsec_ = 0.039;
+    }
     break;
 
   case (TVar::HWW170):
     NLOXsec_ = 0.782962;
-    MCFMXsec_ = 0.031889;
+    MCFMXsec_ = 0.0269;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.782962;
+      MCFMXsec_ = 0.036;
+    }
     break;
 
   case (TVar::HWW180):
     NLOXsec_ = 0.659328;
-    MCFMXsec_ = 0.027389;
+    MCFMXsec_ = 0.0225;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.659328;
+      MCFMXsec_ = 0.030;
+    }
     break;
 
   case (TVar::HWW190):
     NLOXsec_ = 0.486486;
-    MCFMXsec_ = 0.020545;
+    MCFMXsec_ = 0.0164;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.486486;
+      MCFMXsec_ = 0.0222;
+    }
     break;
 
   case (TVar::HWW200):
     NLOXsec_ = 0.408305;
-    MCFMXsec_ = 0.017441;
-    break;
-
-  case (TVar::HWW210):
-    NLOXsec_ = 0.358465;
-    MCFMXsec_ =  0.015340;
-    break;
-    
-  case (TVar::HWW220):
-    NLOXsec_ = 0.321398;
-    MCFMXsec_ = 0.013686;
-    break;
-
-  case (TVar::HWW230):
-    NLOXsec_ = 0.290454;
-    MCFMXsec_ = 0.0123;
+    MCFMXsec_ = 0.0136;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.408305;
+      MCFMXsec_ = 0.0185;
+    }
     break;
 
   case (TVar::HWW250):
     NLOXsec_ = 0.243724;
-    MCFMXsec_ = 0.010063;
+    MCFMXsec_ = 0.007;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.243724;
+      MCFMXsec_ = 0.0097;
+    }
     break;
     
   case (TVar::HWW300):
     NLOXsec_ = 0.175652;
-    MCFMXsec_ = 0.006398;
+    MCFMXsec_ = 0.004024;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.175652;
+      MCFMXsec_ = 0.0057;
+    }
     break;
 
   case (TVar::HWW350):
     NLOXsec_ = 0.160052;
-    MCFMXsec_ = 0.004251;
+    MCFMXsec_ = 0.002439;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.160052;
+      MCFMXsec_ = 0.00353;
+    }
     break;
 
   case (TVar::HWW400):
     NLOXsec_ = 0.124330;
-    MCFMXsec_ = 0.002464;
+    MCFMXsec_ = 0.0013;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.124330;
+      MCFMXsec_ = 0.00192;
+    }
     break;
 
   case (TVar::HWW450):
     NLOXsec_ = 0.078433;
-    MCFMXsec_ = 0.001637;
+    MCFMXsec_ = 0.0008;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.124330;
+      MCFMXsec_ = 0.0012;
+    }
     break;
 
   case (TVar::HWW500):
     NLOXsec_ = 0.048702;
-    MCFMXsec_ = 0.00156;
+    MCFMXsec_ = 0.00052;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.048702;
+      MCFMXsec_ = 0.0008;
+    }
     break;
 
   case (TVar::HWW550):
     NLOXsec_ = 0.030364;
-    MCFMXsec_ = 0.000848;
+    MCFMXsec_ = 0.000356;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.030364;
+      MCFMXsec_ = 0.000559;
+    }
     break;
 
   case (TVar::HWW600):
     NLOXsec_ = 0.019184;
-    MCFMXsec_ = 0.000633;
+    MCFMXsec_ = 0.00025;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.019184;
+      MCFMXsec_ = 0.0004;
+    }
+    break;
+
+  case (TVar::HWW700):
+    NLOXsec_ = 0.019184;
+    MCFMXsec_ = 0.000126;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.019184;
+      MCFMXsec_ = 0.0002;
+    }
+    break;
+
+  case (TVar::HWW800):
+    NLOXsec_ = 0.019184;
+    MCFMXsec_ = 0.000065;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.019184;
+      MCFMXsec_ = 0.00011;
+    }
+    break;
+
+  case (TVar::HWW900):
+    NLOXsec_ = 0.019184;
+    MCFMXsec_ = 0.000034;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.019184;
+      MCFMXsec_ = 0.00006;
+    }
+    break;
+
+  case (TVar::HWW1000):
+    NLOXsec_ = 0.019184;
+    MCFMXsec_ = 0.000018;
+    if ( EightTeV ) {
+      NLOXsec_ = 0.019184;
+      MCFMXsec_ = 0.000033;
+    }
     break;
     
   case (TVar::HZZ250):
