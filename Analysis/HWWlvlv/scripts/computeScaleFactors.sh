24d8b1ed52d6d1d519493d8db7fd440467cd6275
ceballos <>
2012-06-19 18:31:45 +0000

    minor change adding more commands

--- Analysis/HWWlvlv/scripts/computeScaleFactors.sh
+++ Analysis/HWWlvlv/scripts/computeScaleFactors.sh
@@ -1,6 +1,11 @@
 
 #grep -e data: -e S: Smurf/Analysis/HWWlvlv/headers/log_ww_sel.txt| awk '{if(NR%2==1)printf("%d ",$2);else printf("%f %f\n",$2,$4);}'| awk '{if(NR%7==1)a="ll";else if(NR%7==2)a="SF";else if(NR%7==3)a="OF";else if(NR%7==4)a="mm";else if(NR%7==5)a="em";else if(NR%7==6)a="me";else if(NR%7==0)a="ee";if(NR<=7)b="0j";else if(NR<=14)b="1j";else if(NR<=21)b="2j";else if(NR<=28)b="0j";else if(NR<=35)b="1j";else if(NR<=42)b="2j";printf("chan(%2s-%2s) ==> data: %4d S: %7.2f B: %7.2f -> %5.3f +/- %5.3f\n",b,a,$1,$2,$3,($1-$3)/$2,sqrt($1)/$2);}'
 
+#same-sign, 5 final states, data, bkg, qqww, ggww, top, wjets, vv, zjets, vg
+#grep -e data -e "bdg(27)" -e "bdg(28)" -e "bdg(29)" -e "bdg(30)" -e "bdg(xW)" -e "bdg(xZ)" -e "bdg(tX)" -e "bdg(Vg)" ../../Smurf/Analysis/HWWlvlv/headers/log_ss_ww_20_20.txt|awk '{if($1!="data:")printf("%8.3f %8.3f ",$3,$5);else printf("%4d ",$2);if(NR%9==0)printf("\n");}' > kkk
+#awk '{printf("    & %4d & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f & %6.1f $\\pm$ %5.1f\n",$1,($2+$4+$6+$8+$10+$12+$14+$16),sqrt($3*$3+$5*$5+$7*$7+$9*$9+$11*$11+$13*$13+$15*$15*$17*$17),$6,$7,$8,$9,$14,$15,$10,$11,($2+$4),sqrt($3*$3+$5*$5),$12,$13,$16,$17)}' kkk
+
+
 export NSEL=$1;
 
 if [ $1 == 1 ]; then
