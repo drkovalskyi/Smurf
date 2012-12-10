20e461155d18abf870d049d234de78c46712913a
yygao <>
2012-12-10 13:01:36 +0000

    from /tas/jaehyeok/home/HWW/Analysis/Combination/CMSSW_5_3_2_patch4/src/

--- LimitCalc/mlfitNormsToText.py
+++ LimitCalc/mlfitNormsToText.py
@@ -15,12 +15,23 @@ fit_b = file.Get("norm_fit_b")
 if fit_s == None: raise RuntimeError, "Missing fit_s in %s. Did you run MaxLikelihoodFit with --saveNorm?" % file;
 if fit_b == None: raise RuntimeError, "Missing fit_b in %s. Did you run MaxLikelihoodFit with --saveNorm?" % file;
 
+print "--------------------------------"
+print " Process    post-fit yields "
+print "--------------------------------"
+
+norm_s_all=0
 iter = fit_s.createIterator()
 while True:
     norm_s = iter.Next()
     if norm_s == None: break;
     norm_b = fit_b.find(norm_s.GetName())
-    m = re.match(r"n_exp_bin(\w+)_proc_(\w+)", norm_s.GetName());
+    m = re.match("n_exp_(\w+)_proc_(\w+)", norm_s.GetName());
     if m == None: raise RuntimeError, "Non-conforming object name %s" % norm_s.GetName()
     if norm_b == None: raise RuntimeError, "Missing normalization %s for background fit" % norm_s.GetName()
-    print "%-30s %-30s %7.3f %7.3f" % (m.group(1), m.group(2), norm_s.getVal(), norm_b.getVal())
+    #print "%-30s %-30s %7.3f %7.3f" % (m.group(1), m.group(2), norm_s.getVal(), norm_b.getVal())
+    print " %-15s %7.3f" % (m.group(2), norm_s.getVal()) 
+    norm_s_all = norm_s_all + norm_s.getVal()
+
+print "-------------------------------"
+print "Sum All      %7.3f" % norm_s_all 
+print "-------------------------------"
