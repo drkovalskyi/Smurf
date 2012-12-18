2364fa42abd53b875429ca612c23d25252ab49ff
ceballos <>
2012-12-18 09:48:35 +0000

    first moriond update

--- Analysis/HWWlvlv/TopBkgScaleFactors_8TeV.h
+++ Analysis/HWWlvlv/TopBkgScaleFactors_8TeV.h
@@ -1,12 +1,12 @@
 static Double_t TopBkgScaleFactor(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactor[3] = { 1.02808, 1.0584, 1.01447   };
+  Double_t TopBkgScaleFactor[3] = { 1.0172, 1.04882, 1.04211   };
   return TopBkgScaleFactor[jetBin];
 }
 
 static Double_t TopBkgScaleFactorKappa(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactorKappa[3] = { 1.19626, 1.04005, 1.0263   };
+  Double_t TopBkgScaleFactorKappa[3] = { 1.19517, 1.03188, 1.0259   };
   return TopBkgScaleFactorKappa[jetBin];
 }
 
