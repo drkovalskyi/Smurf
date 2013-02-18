db12d135f5897d4afba0e79a5525842c1074f6ee
ceballos <>
2013-02-18 13:00:06 +0000

    new scale factors

--- Analysis/HWWlvlv/TopBkgScaleFactors_8TeV.h
+++ Analysis/HWWlvlv/TopBkgScaleFactors_8TeV.h
@@ -1,12 +1,12 @@
 static Double_t TopBkgScaleFactor(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactor[3] = { 1.05659, 1.07258, 1.08572   };
+  Double_t TopBkgScaleFactor[3] = { 1.05519, 1.07248, 1.08536   };
   return TopBkgScaleFactor[jetBin];
 }
 
 static Double_t TopBkgScaleFactorKappa(Int_t jetBin) {
   assert(jetBin >=0 && jetBin <= 2);
-  Double_t TopBkgScaleFactorKappa[3] = { 1.19245, 1.03155, 1.02578   };
+  Double_t TopBkgScaleFactorKappa[3] = { 1.19259, 1.03155, 1.02578   };
   return TopBkgScaleFactorKappa[jetBin];
 }
 
