02bf0585bb64d35365069347c90b10b298dbcfa7
Guillelmo Gomez Ceballos Retuerto <ceballos@dtmit02.cern.ch>
2014-01-24 15:14:23 +0100

    first real commit

--- ProcessingAndSkimming/crab/checkLumi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RUNLIST=Cert_190456-202305_8TeV_PromptReco_Collisions12_JSON.txt
-JOB=$1
-
-compareJSON.py --and ${JOB}/res/lumiSummary.json ${RUNLIST} > processed_${JOB1}.json
-lumiCalc2.py -i processed_${JOB1}.json overview
-
--- ProcessingAndSkimming/crab/getAllReports.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for DIR in `ls | grep ^RemoteGlidein`;
-do
-    echo crab -report -c $DIR
-done
-
